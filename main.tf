5ef45637de4d0e3616de828d9e4c0940f88dc686
cloud2312 <cloudexpert2312@gmail.com>
2026-08-04 21:45:09 +0530

    Initial clean commit for Terraform demo

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
