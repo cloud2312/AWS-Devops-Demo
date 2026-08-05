1971947bf2f397e0be1b8b0302b32c850a9c158f
cloud2312 <cloudexpert2312@gmail.com>
2026-08-05 13:22:58 +0530

    Modify S3 Bucket

--- S3.tf
+++ S3.tf
@@ -4,10 +4,15 @@ resource "random_id" "bucket_id" {
 
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "aws-devops-demo-bucket-${random_id.bucket_id.hex}"
-  acl    = "private"   # ⚠️ Deprecated, consider aws_s3_bucket_acl
 
   tags = {
     Name        = "devops-demo-bucket"
     Environment = "dev"
   }
+}
+
+# New resource for ACL
+resource "aws_s3_bucket_acl" "demo_bucket_acl" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  acl    = "private"
 }
