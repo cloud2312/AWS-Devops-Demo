2f2ac37bb7132b1b11f17dc9db429576ff828c0c
cloud2312 <cloudexpert2312@gmail.com>
2026-08-05 20:38:56 +0530

    added subnet details

---
+++ subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-subnet-a"
+  }
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-subnet-b"
+  }
+}
